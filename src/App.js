46a49b5bd08871b9ed275521d8ca014f38b1080e
TheJmqn <jman4461@gmail.com>
2023-04-16 09:29:49 -0400

    Add ball collision and make screen relative

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { useRef, useEffect } from 'react';
 
-class Line {
+const canvasWidth = window.innerWidth-16;
+const canvasHeight = window.innerHeight;
+
+class HorziontalLine {
   constructor(x, y, length) {
-    this.x = x;
-    this.y = y;
-    this.length = length;
+    this.x = percentWidth(x);
+    this.y = percentHeight(y);
+    this.length = percentWidth(length);
   }
 
   draw(ctx) {
@@ -23,12 +26,12 @@ class Line {
 }
 
 class Ball {
-  constructor(x, y, radius, dx, dy) {
+  constructor(x, y, radius, speed) {
     this.x = x;
     this.y = y;
     this.radius = radius;
-    this.dx = dx;
-    this.dy = dy;
+    this.dx = percentWidth(speed);
+    this.dy = percentHeight(speed);
   }
 
   draw(ctx) {
@@ -57,11 +60,12 @@ class Ball {
 }
 
 class PlayerBall {
-  constructor(speed, x, y, radius, canvasWidth, canvasHeight, lines) {
-    this.speed = speed;
-    this.x = x;
-    this.y = y;
-    this.radius = radius;
+  constructor(speed, x, y, radius, lines) {
+    this.dx = percentWidth(speed);
+    this.dy = percentHeight(speed);
+    this.x = percentWidth(x);
+    this.y = percentHeight(y);
+    this.radius = percentWidth(radius);
     this.up = false;
     this.left = false;
     this.down = false;
@@ -76,13 +80,13 @@ class PlayerBall {
     ctx.arc(this.x, this.y, this.radius, 0, Math.PI * 2);
     ctx.fillStyle = 'red';
     if(this.up) 
-      this.y -= this.speed;
+      this.y -= this.dy;
     if(this.left)
-      this.x -= this.speed;
+      this.x -= this.dx;
     if(this.down)
-      this.y += this.speed;
+      this.y += this.dy;
     if(this.right)
-      this.x += this.speed;
+      this.x += this.dx;
     this.stayInBounds();
     this.linesCollision();
     ctx.fill();
@@ -136,7 +140,7 @@ class PlayerBall {
     }
   }
 
-  linesCollision(){
+  linesCollision() {
     for (let i = 0; i < this.lines.length; i++) {
       const line = this.lines[i];
       if (this.y - this.radius < line.y && this.y + this.radius > line.y && this.x + this.radius > line.x && this.x - this.radius < line.x + line.length) {
@@ -148,31 +152,38 @@ class PlayerBall {
       }
     }
   }
+
+  ballCollision(otherBall) {
+    const dx = this.x - otherBall.x;
+    const dy = this.y - otherBall.y;
+    const distance = Math.sqrt(dx * dx + dy * dy);
+    return distance < this.radius + otherBall.radius;
+  }
 }
 
 function Canvas() {
   const canvasRef = useRef(null);
   const balls = [
-    new Ball(50, 50, 5, 2, 2),
-    new Ball(100, 100, 10, 3, 3),
-    new Ball(200, 200, 20, 4, 4)
+    new Ball(50, 50, 5, 0.3),
+    new Ball(100, 100, 10, 0.2),
+    new Ball(200, 200, 20, 0.25)
   ];
   const lines = [
-    new Line(0, 300, 200),
-    new Line(400, 100, 150)
+    new HorziontalLine(5, 10, 40),
+    new HorziontalLine(50, 70, 20),
+    new HorziontalLine(50, 40, 30)
   ];
-  const playerBall = new PlayerBall(2, 50, 50, 20, 800, 600, lines);
+  const playerBall = new PlayerBall(0.25, 50, 50, 1, lines);
 
   useEffect(() => {
     const canvas = canvasRef.current;
     const ctx = canvas.getContext('2d');
     
-    // Set canvas size
-    canvas.width = 800;
-    canvas.height = 600;
+    canvas.width = canvasWidth;
+    canvas.height = canvasHeight;
 
     function clearCanvas() {
-      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      ctx.clearRect(0, 0, canvasWidth, canvasHeight);
     }
 
     function draw() {
@@ -182,6 +193,9 @@ function Canvas() {
       }
       for (let i = 0; i < balls.length; i++) {
         balls[i].draw(ctx);
+        if (playerBall.ballCollision(balls[i])) {
+          console.log('Player ball collided with another ball!');
+        }
       }
       playerBall.draw(ctx);
     }
@@ -190,6 +204,13 @@ function Canvas() {
       for (let i = 0; i < balls.length; i++) {
         balls[i].move(canvas, lines);
       }
+    }
+    
+    function intersects(circle1, circle2) {
+      const dx = circle1.x - circle2.x;
+      const dy = circle1.y - circle2.y;
+      const distance = Math.sqrt(dx * dx + dy * dy);
+      return distance < circle1.radius + circle2.radius;
     }
 
     document.addEventListener('keypress', (event) => {
@@ -238,8 +259,16 @@ function Canvas() {
   }, []);
 
   return (
-    <canvas ref={canvasRef} style={{ border: '1px solid black' }} />
+    <canvas ref={canvasRef}/>
   );
+}
+
+function percentWidth(num) {
+  return num*0.01*canvasWidth;
+}
+
+function percentHeight(num) {
+  return num*0.01*canvasHeight;
 }
 
 export default Canvas;
